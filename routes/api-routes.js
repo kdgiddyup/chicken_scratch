7fac5f81fba1b649b3d69e4f74fe61d8c912cc51
Katharina Fuetterer <Katharina Fuetterer>
2017-05-13 08:57:43 -0400

    Update Api Routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,50 +1,61 @@
 var db = require("../models");
 
-//GET ALL STORIES AND BLURBS
-// router.get("/", function(req, res) {
+module.exports = function(app){
 
-// })
+    //GET ALL STORIES AND BLURBS
+    app.get("/", function(req, res) {
+        db.Story.findAll({}).then(function(results) {
+        var hbsObject = {
+            stories: results
+        };
+        res.render("index", hbsObject);
+        });
+    })
 
-// //GET ALL CONTRIBUTIONS FOR EACH STORY AND USER ASSOCIATED WITH EACH CONTRIBUTION
-// router.get("/", function(req, res) {
+    // //GET ALL CONTRIBUTIONS FOR EACH STORY AND USER ASSOCIATED WITH EACH CONTRIBUTION
+    app.get("/", function(req, res) {
+        db.Contribution.findAll({}).then(function(results) {
+        var hbsObject = {
+            contributions: results
+        };
+        res.render("index", hbsObject);
+        });
+    });
 
-// });
+    // //CREATE USER
+    // app.post("/", function(req, res) {
+    //   db.User.create({
+    //     username: req.body.username,
+    //     password: req.body.password
+    //   }).then(function(results) {
+    //     res.end();
+    //   });
+    // });
 
-// //CREATE USER
-// router.post("/", function(req, res) {
-//   db.User.create({
-//     username: req.body.username,
-//     password: req.body.password
-//   }).then(function(results) {
-//     res.end();
-//   });
-// });
+    // //CREATE CONTRIBUTION
+    // app.post("/", function(req, res) {
+    //   db.Contribution.create({
+    //     contribution_text: req.body.contribution_text
+    //   }).then(function(results) {
+    //     res.end();
+    //   });
+    // });
 
-// //CREATE CONTRIBUTION
-// router.post("/", function(req, res) {
-//   db.Contribution.create({
-//     contribution_text: req.body.contribution_text
-//   }).then(function(results) {
-//     res.end();
-//   });
-// });
+    // //ADD RANK
+    // app.put("/:id", function(req, res) {
+    //     db.Contribution.update({
+    //         rank: req.body.rank
+    //     }).then(function(results) {
+    //         res.end();
+    //     })
+    // });
 
-// //ADD RANK
-// router.put("/:id", function(req, res) {
-//     db.Contribution.update({
-//         rank: req.body.rank
-//     }).then(function(results) {
-//         res.end();
-//     })
-// });
-
-// //ADD RANK
-// router.put("/:id", function(req, res) {
-//     db.Art.update({
-//         rank: req.body.rank
-//     }).then(function(results) {
-//         res.end();
-//     })
-// });
-
-module.exports = function(app){};
+    // //ADD RANK
+    // app.put("/:id", function(req, res) {
+    //     db.Art.update({
+    //         rank: req.body.rank
+    //     }).then(function(results) {
+    //         res.end();
+    //     })
+    // });
+};

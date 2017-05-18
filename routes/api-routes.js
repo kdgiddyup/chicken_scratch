351c801fa6ef1d4bc7698a13309cbf6674989bda
Kelly Davis <kellydavis1974@gmail.com>
2017-05-18 03:48:54 -0400

    have successful file transfer to AWS and URL placement in sql database

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,31 +1,28 @@
 var db = require("../models");
 
-// module for handling multipart file uploads -- ie, images
-var multer  = require('multer')
+// set up for image uploading
+// multer helps with multipart files (ie, images)
+var multer = require('multer');
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
+
+var S3FS = require('s3fs');
+var bucketPath = process.env.S3_BUCKET;
+
+var s3Options = {
+  region: 'us-east-1',
+};
+var fsImpl = new S3FS(bucketPath, s3Options);
 
 
 // for aws storage, require the aws sdk
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+    // var AWS = require('aws-sdk');
+    // var s3 = new AWS.S3();
 // For dev purposes only
-AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
+    // AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
 
-var bucket = new AWS.S3({params: {Bucket: process.env.S3_BUCKET}});
+    // var bucket = new AWS.S3({params: {Bucket: process.env.S3_BUCKET}});
 
-// method for uploading images ** https://gist.github.com/adon-at-work/26c8a8e0a1aee5ded03c **
-function uploadToS3(file, destFileName, callback) {
-    bucket
-        .upload({
-            ACL: 'public-read', 
-            Body: fs.createReadStream(file.path), 
-            Key: destFileName.toString(),
-            ContentType: 'application/octet-stream' // force download if it's accessed as a top location
-        })
-        // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3/ManagedUpload.html#httpUploadProgress-event
-        // .on('httpUploadProgress', function(evt) { console.log(evt); })
-        // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3/ManagedUpload.html#send-property
-        .send(callback);
-}
 
 module.exports = function(app){
 
@@ -46,10 +43,10 @@ module.exports = function(app){
             },
             include: [db.Story]
         }).then(function(data) {
-              console.log(data);
+            console.log(data);
             res.render("story", {contributions: data});
+            });
         });
-    });
 
     //SEARCH USER
     app.get("/", function(req, res) {
@@ -95,57 +92,24 @@ module.exports = function(app){
     });
 
     // upload art; id param here is contribution id 
-    app.post("/api/new/art/:id", function (req, res) {
-        
-        if (!req.files || !req.files.file1) {
-            return res.status(403).json({message:'Expect 1 file upload'});
-        }
-        var file1 = req.files.file1;
-
-        // this is mainly for user friendliness. this field can be freely tampered by attacker.
-        if (!/^image\/(jpe?g|png|gif)$/i.test(file1.mimetype)) {
-            return res.status(403).json({message:'Must upload image file type'});
-        }
-
-        var pid = '10000' + parseInt(Math.random() * 10000000);
-
-        uploadToS3(file1, pid, function (err, data) {
-            if (err) {
-                console.error(err);
-            }
-            console.log('Image uploaded to',data.Location);
-            
+    app.post("/api/new/art", upload.single('fileupload'), function (req, res, next) {
+    // req.file is the `fileupload` file 
+    // req.body will hold the text fields, if there were any   
+       var fileName = "img-Story"+req.body.StoryId+"-Contrib"+req.body.ContributionId+"."+req.file.mimetype.split("/")[1];
+       console.log(req.file);
+        fsImpl.writeFile(fileName, req.file.buffer, "binary", function (err) {
+            if (err) throw(err);
+            db.Art.create({
+                art_file: 'https://s3.amazonaws.com/chickenscratchdb/'+fileName,
+                ContributionId: req.body.ContributionId,
+                StoryId: req.body.StoryId
+            }).then(function(results) {
+                res.redirect("/story/"+req.body.StoryId)
+            })    
         });
     });
-
+ 
         
-    
-            
-            // Read in the file, convert it to base64, store to S3
-            // fs.readFile(req.body.filePath, function (err, data) {
-            // if (err) { throw err; }
-
-            // var base64data = new Buffer(data, 'binary');
-
-            // var s3 = new AWS.S3();
-            // s3.client.putObject({
-            //     Bucket: process.env.S3_BUCKET,
-            //     Key: req.body.filePath,
-            //     Body: base64data,
-            //     ACL: 'public-read'
-            // },function (resp) {
-            //     console.log(arguments);
-            //     console.log('Successfully uploaded package.');
-            // });
-
-            // });
-
-
-        
-        // db.Art.update({  // to do: add contribution and story id values
-        //     art_file: req.body.art_file 
-        // })
-
 
    //ADD RANK
     // app.put("/:id", function(req, res) {

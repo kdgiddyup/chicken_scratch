e354c8cc31622f0cc588477d8e370e40a65c89ae
Maria Gabriela Cortez <mariagabrielacortez@lawn-128-61-17-146.lawn.gatech.edu>
2017-05-18 21:44:41 -0400

    update1

--- models/stories.js
+++ models/stories.js
@@ -1,18 +1,16 @@
-
 module.exports = function(sequelize, DataTypes) {
-  var Story = sequelize.define("Story", {
-    title: DataTypes.STRING,
-    blurb: DataTypes.STRING,
-    rank: DataTypes.STRING,
-    cover_art: {type: DataTypes.STRING, defaultValue:'https://s3.amazonaws.com/chickenscratchdb/default.jpg'}
-  },
-  {
-    classMethods: {
-      associate: function(models) {
-        Story.hasMany(models.Contribution);
-        Story.hasMany(models.Art);
-      }
-    }
-  });
-  return Story;
+    var Story = sequelize.define("Story", {
+        title: DataTypes.STRING,
+        blurb: DataTypes.STRING,
+        rank: DataTypes.STRING,
+        cover_art: { type: DataTypes.STRING, defaultValue: './assets/media/3.jpg' }
+    }, {
+        classMethods: {
+            associate: function(models) {
+                Story.hasMany(models.Contribution);
+                Story.hasMany(models.Art);
+            }
+        }
+    });
+    return Story;
 };
